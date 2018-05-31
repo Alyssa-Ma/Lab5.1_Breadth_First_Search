5fef09b17daa120568d393e4c1b0369d658d0efd
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-05-31 09:19:15 -0400

    set up classes

--- Lab5.1/src/BreadthFirstSearch.java
+++ Lab5.1/src/BreadthFirstSearch.java
@@ -1,4 +1,35 @@
+/**\
+ * 
+ * @author Alyssa Ma
+ * 5/31/18
+ *
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch {
+	Node startNode;
+	Node goalNode;
+	
+	public BreadthFirstSearch(Node start, Node goalNode)
+	{
+		this.startNode = start;
+		this.goalNode = goalNode;
+	}
+	
+	public boolean compute()
+	{
+		if(this.startNode.equals(goalNode))
+		{
+			System.out.println("Goal Node Found.");
+			System.out.println(startNode);
+		}
+		
+		Queue<javax.xml.soap.Node> queue = new LinkedList<>();
+		ArrayList<Node> explored = new ArrayList<>();
+		queue.add(this.startNode);
+		explored.add
+	}
 
 }
--- Lab5.1/src/Runner.java
+++ Lab5.1/src/Runner.java
@@ -1,4 +1,9 @@
-
+/**\
+ * 
+ * @author Alyssa Ma
+ * 5/31/18
+ *
+ */
 public class Runner {
 
 }
